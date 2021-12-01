c6c6996b21f112bf5727b79840bb2232d46cdeb3
SoloVova <vsolo0@gmail.com>
2021-12-01 22:27:28 +0200

    0.06

--- GuiTradeHelper/WindowTestTrade.xaml.cs
+++ GuiTradeHelper/WindowTestTrade.xaml.cs
@@ -1,4 +1,10 @@
-﻿using System.Windows;
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.Windows;
+using Binance.Net.Interfaces;
+using CryptoExchange.Net.Objects;
+using SoloVova.TradeHelper.LibTradeHelper.binance.market.dev.LowApi;
+using Utf8Json;
 
 namespace SoloVova.TradeHelper.GuiTradeHelper{
     public partial class WindowTestTrade : Window{
@@ -8,7 +14,19 @@ namespace SoloVova.TradeHelper.GuiTradeHelper{
         
         private void ButtonRefresh_OnClick(object sender, RoutedEventArgs e)
         {
-            
+            Task<WebCallResult<IEnumerable<IBinanceRecentTrade>>> task = ApiSymbol.GetTradeHistory("BTCUSDT");
+            task.ContinueWith(res =>
+            {
+                this.Dispatcher.Invoke(() => {
+                    if (res.IsCompletedSuccessfully){
+                        this.tb.Text = "Ok\n"+JsonSerializer.ToJsonString(res.Result.Data);    
+                    }
+                    else{
+                        this.tb.Text = "Error:" + res.Exception?.Message;
+                    }
+                    
+                });
+            });
 
         }
     }
